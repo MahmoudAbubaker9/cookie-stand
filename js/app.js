803445f605133bfbda2f12d349523996dc795cd3
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-06-02 18:41:41 +0300

    add form

--- js/app.js
+++ js/app.js
@@ -8,7 +8,21 @@ let hourArr =['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4P
 let table = document.createElement('table')
 let stores = document.getElementById('cookiesSale');
 stores.appendChild(table)
+let inputForm = document.getElementById('newBranch')
 let arrStore = [];
+
+function Branch(locationName,minCust,maxCust,avgCookies){
+    this.locationName = locationName,
+    this.minCust = minCust,
+    this.maxCust = maxCust,
+    this.avgCookies= avgCookies,
+    this.custHour =  [];
+    this.totalRep = 0,
+    this.multy=[];
+    this.sumval = 0,
+    arrStore.push(this)
+    
+}
 
 function timeList(){
  let row1 = document.createElement('tr')
@@ -29,30 +43,16 @@ function timeList(){
 }
 
 
-function Branch(locationName,minCust,maxCust,avgCookies){
-    this.locationName = locationName,
-    this.minCust = minCust,
-    this.maxCust = maxCust,
-    this.avgCookies= avgCookies,
-    this.custHour =  0,
-    this.totalRep = 0,
-    this.multy=[];
-    this.sumval = 0,
-    this.totalofT= 0,
-    arrStore.push(this)
-    
-}
+
 
 Branch.prototype.custnumber = function(){
     for ( let i=0 ; i < hourArr.length ; i++){
-    this.custHour = randomCustomer(this.minCust,this.maxCust)
-    this.multy[i] = Math.floor(this.custHour * this.avgCookies)
+    this.custHour.push(randomCustomer(this.minCust,this.maxCust))
+    this.multy.push(this.custHour[i] * Math.ceil(this.avgCookies));
     this.totalRep = this.totalRep + this.multy[i];
-    
-
 
 }
-console.log(this.totalRep)
+console.log(this.custHour)
 }
 Branch.prototype.render = function(){
    
@@ -71,35 +71,31 @@ Branch.prototype.render = function(){
     row2.appendChild(td11)
     td11.textContent= this.totalRep
 
-    
-
-    
-
 }
 
-Branch.prototype.resultfunc = function(){
+ let totalofT= 0;
+function resultfunc(){
     let row3 = document.createElement('tr')
     table.appendChild(row3)
-    let td = document.createElement('td')
-        td.textContent = 'Totals'
-        row3.appendChild(td)
+    let firstCell = document.createElement('th')
+    row3.appendChild(firstCell)
+    firstCell.textContent = 'Totals'
+    
     for(let y=0 ; y < hourArr.length ; y++ ){
         let td = document.createElement('td')
         row3.appendChild(td)
         let valSum = 0;
         for( let q=0 ; q < arrStore.length ; q++){
-        valSum = valSum + arrStore[q].multy[y]
-    }
-    this.totalofT +=
+        valSum = valSum + arrStore[q].multy[y]  
+           
+       }  
+       totalofT +=
     td.textContent=valSum
-    }
-    
-    let td12 = document.createElement('td')
-    row3.appendChild(td12)
-    td12.textContent= this.totalofT
-
-},
-
+   }
+   let td12 = document.createElement('td')
+   row3.appendChild(td12)
+   td12.textContent= totalofT
+};
 
 
 timeList()
@@ -108,17 +104,41 @@ let Tokyo = new Branch('Tokyo',3,24,1.2)
 let Dubai = new Branch('Dubai',11,38,3.7)
 let Paris = new Branch('Paris',20,38,2.3)
 let Lima = new Branch('Lima',2,16,4.6)
-Seattle.custnumber()
-Seattle.render()
-Tokyo.custnumber()
-Tokyo.render()
-Dubai.custnumber()
-Dubai.render()
-Paris.custnumber()
-Paris.render()
-Lima.custnumber()
-Lima.render()
-Lima.resultfunc()
+
+
+for ( let w = 0 ; w < arrStore.length ; w++){
+arrStore[w].custnumber()
+arrStore[w].render()
+}
+
+resultfunc()
+console.log(arrStore);
+
+inputForm.addEventListener('submit',inputFunction)
+
+function inputFunction(event){
+    event.preventDefault()
+    let nameForm = event.target.nameInput.value
+    console.log(event.target.nameInput.value);
+    let minForm = event.target.minInput.value
+    console.log(event.target.minInput.value);
+    let maxForm = event.target.maxInput.value
+    console.log(event.target.maxInput.value);
+    let avgForm = event.target.avgInput.value
+    console.log(event.target.avgInput.value);
+    let inputData = new Branch (nameForm,minForm,maxForm,avgForm)
+
+    inputData.custnumber()
+    inputData.render()
+
+    resultfunc()
+
+}
+inputFunction()
+
+
+
+
 
 
 // // ------------------------------------
